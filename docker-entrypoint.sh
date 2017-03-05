066b397aa924d1740946d9dee89a9ce381ed2188
vromero <victor.romero@gmail.com>
2017-03-05 14:24:39 -0300

    Fix for #6 without changing dockerfile and therefor having an estable /etc, to not to interfere with clustering

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ sed -i "s/logger.handlers=.*/logger.handlers=CONSOLE/g" ../etc/logging.propertie
 
 # Update users and roles with if username and password is passed as argument
 if [[ "$ARTEMIS_USERNAME" && "$ARTEMIS_PASSWORD" ]]; then
-  sed -i "s/amq=artemis/$ARTEMIS_USERNAME=amq/g" ../etc/artemis-roles.properties
-  sed -i "s/artemis=simetraehcapa/$ARTEMIS_USERNAME=$ARTEMIS_PASSWORD/g" ../etc/artemis-users.properties
+  sed -i "s/amq[ ]*=.*/$ARTEMIS_USERNAME=amq\n/g" ../etc/artemis-roles.properties
+  sed -i "s/artemis[ ]*=.*/$ARTEMIS_USERNAME=$ARTEMIS_PASSWORD\n/g" ../etc/artemis-users.properties
 fi
 
 # Update min memory if the argument is passed
