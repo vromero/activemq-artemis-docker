13151aaa0a19a729f6b0b774f52079af4a7c5a80
vromero <victor.romero@gmail.com>
2017-03-07 16:58:28 -0300

    Support for etc overwrite

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,6 +20,21 @@ if [[ "$ARTEMIS_MAX_MEMORY" ]]; then
   sed -i "s/-Xmx1024M/-Xmx$ARTEMIS_MAX_MEMORY/g" ../etc/artemis.profile
 fi
 
+if [ -f /var/lib/artemis/etc-override/custom-transformations.xslt ]; then
+  echo Applying custom-transformations.xslt
+  cp /var/lib/artemis/etc/broker.xml /tmp/broker.xml
+  xmlstarlet tr /var/lib/artemis/etc-override/custom-transformations.xslt /tmp/broker.xml > /var/lib/artemis/etc/broker.xml
+else
+  echo No custom-transformations.xslt found
+fi
+
+if [ -f /var/lib/artemis/etc-override/broker.xml ]; then
+  cp /var/lib/artemis/etc/broker.xml /tmp/broker.xml
+  xmlstarlet tr /opt/merge/merge.xslt -s with=/var/lib/artemis/etc-override/broker.xml /tmp/broker.xml > /var/lib/artemis/etc/broker.xml
+else
+  echo No broker.xml override found
+fi
+
 if [ "$1" = 'artemis-server' ]; then
 	set -- gosu artemis "./artemis" "run"
 fi
