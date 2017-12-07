828347d5a128c9568f64aa217224d2c8fdecc2a9
Jarek Przygódzki <jarek.przygodzki@gmail.com>
2017-12-07 12:07:38 +0100

    Always respect ARTEMIS_MIN_MEMORY & ARTEMIS_MAX_MEMORY
    This closes #25

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,12 +16,12 @@ fi
 
 # Update min memory if the argument is passed
 if [[ "$ARTEMIS_MIN_MEMORY" ]]; then
-  sed -i "s/-Xms512M/-Xms$ARTEMIS_MIN_MEMORY/g" ../etc/artemis.profile
+  sed -i "s/-Xms[^ ]*/-Xms$ARTEMIS_MIN_MEMORY/g" ../etc/artemis.profile
 fi
 
 # Update max memory if the argument is passed
 if [[ "$ARTEMIS_MAX_MEMORY" ]]; then
-  sed -i "s/-Xmx1024M/-Xmx$ARTEMIS_MAX_MEMORY/g" ../etc/artemis.profile
+  sed -i "s/-Xmx[^ ]*/-Xmx$ARTEMIS_MAX_MEMORY/g" ../etc/artemis.profile
 fi
 
 files=$(find $OVERRIDE_PATH -name "broker*" -type f | cut -d. -f1 | sort -u );
